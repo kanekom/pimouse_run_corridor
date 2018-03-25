0fbe4fa30122de07e547321ace134689cd1f3301
Mitsuhisa Kaneko <kanekom@ya2.so-net.ne.jp>
2018-03-25 09:26:50 +0000

    add wall around script & test

---
+++ test/travis_test_wall_around.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import unittest, rostest
+import rosnode, rospy
+import time
+
+class WallAroundTest(unittest.TestCase):
+    def set_and_get(self,lf,ls,rs,rf):
+        with open("/dev/rtlightsensor0","w") as f:
+            f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
+       
+        time.sleep(0.5)
+
+        with open("/dev/rtmotor_raw_l0","r") as lf,\
+             open("/dev/rtmotor_raw_r0","r") as rf:
+            left = int(lf.readline().rstrip())
+            right = int(rf.readline().rstrip())
+
+        return left, right
+
+    def test_io(self):
+        left, right = self.set_and_get(400,0,0,100) #curve to right
+        self.assertTrue(left > right == 0,"don't curve to left : " + str(left) + "," + str(right))
+        
+        left, right = self.set_and_get(0,5,1000,0) #curve to left
+        self.assertTrue(left < right != 0,"don't curve to right : " + str(left) + "," + str(right))
+
+        left, right = self.set_and_get(0,100,0,0) #curve to  right
+        self.assertTrue(left > right, "don't curve to left" + str(left) + "," + str(right))
+
+        left, right = self.set_and_get(5,5,5,5) #straight
+        self.assertTrue(0 < left == right, "curve wrongly : " + str(left) + ',' + str(right))
+       
+if __name__ == '__main__':
+    time.sleep(3)
+    rospy.init_node('travis_test_wall_stop')
+    rostest.rosrun('pimouse_run_corridor','travis_test_wall_around',WallAroundTest)
