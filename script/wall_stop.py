157e3d5b2461b4d833484e7618d179821b0cc5db
Mitsuhisa Kaneko <kanekom@ya2.so-net.ne.jp>
2018-03-25 05:57:57 +0000

    add wall stop script2

--- script/wall_stop.py
+++ script/wall_stop.py
@@ -5,7 +5,7 @@ from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
 class WallStop():
-    def __init__(self)
+    def __init__(self):
         self.cmd_vel = rospy.Publisher('cmd_vel',Twist,queue_size=1)
 
         self.sensor_values = LightSensorValues()
@@ -28,5 +28,5 @@ if __name__ == '__main__':
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
-    rospy.ServiceProsy('/motor_on',Trigger).call()
+    rospy.ServiceProxy('/motor_on',Trigger).call()
     WallStop().run()
